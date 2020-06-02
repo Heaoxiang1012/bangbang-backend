0ff032bc2e5ae4d79f2c48ab45f58151ac1b8689
StubbornStole <60861712+StubbornStole@users.noreply.github.com>
2020-06-02 20:04:35 +0800

    Update note.py

--- backend/models/note.py
+++ backend/models/note.py
@@ -6,14 +6,14 @@ class Note(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(32), nullable=False)  # 标题
-    compliments = db.Column(db.Integer, default=0)  # 点赞数目
+    compliments = db.Column(db.Integer, default=0)  # 点赞数
     note_date = db.Column(db.TIMESTAMP, nullable=False)  # 笔记最近一次更新时间
     tag = db.Column(db.String(32),nullable=False)
     content = db.Column(db.Text)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  #谁发布的
 
-    user = db.relationship('User', back_populates='notes')  # 用户User
+    user = db.relationship('User', back_populates='notes')  # 用户
     comps = db.relationship('Compliments', back_populates='note')
 
 
