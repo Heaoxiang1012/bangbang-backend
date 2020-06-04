6b886e91248eead366c72ed44bee9310746e1f49
Heaoxiang1012 <921269066@qq.com>
2020-06-04 20:40:43 +0800

    管理接口调整模块

--- backend/blueprints/help.py
+++ backend/blueprints/help.py
@@ -63,6 +63,7 @@ def release():
         skill_name = request.form.get('skill_name')
         help.file_id = file_id
         help.skill_name = skill_name
+        help.type = 'skill'
         db.session.add(help)
         db.session.commit()
 
